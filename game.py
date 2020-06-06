541f120d290b0b1cad09caf9c3ab61977e13065f
Muhammad Sulaiman <mksuleiman03@gmail.com>
2020-06-06 15:16:07 +0100

    few changes

--- game.py
+++ game.py
@@ -11,22 +11,17 @@ robot_choice = random.choice(options)
 
 # user results
 if user_choice == "rock" and robot_choice == "scissors":
-    print("robot choice is:")
-    print(robot_choice)
+    print(f"robot choice is: {robot_choice}")
     print("You win!")
 elif user_choice == "paper" and robot_choice == "rock":
-    print("robot choice is:")
-    print(robot_choice)
+    print(f"robot choice is: {robot_choice}")
     print("You win!")
 elif user_choice == "scissors" and robot_choice == "paper":
-    print("robot choice is:")
-    print(robot_choice)
+    print(f"robot choice is: {robot_choice}")
     print("You win!")
 elif user_choice == robot_choice:
-    print("robot choice is:")
-    print(robot_choice)
+    print(f"robot choice is: {robot_choice}")
     print("draw")
 else:
-    print("robot choice is:")
-    print(robot_choice)
+    print(f"robot choice is: {robot_choice}")
     print("You lose :(")
